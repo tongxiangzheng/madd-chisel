d3b7c14bc103b47d4f7e9352aded9bb3a23b51f7
txz <920504276@qq.com>
2023-05-03 14:51:06 +0800

    prefetch init

---
+++ src/main/scala/madd/prefetchTester.scala
@@ -0,0 +1,25 @@
+package madd
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import chisel3.util._
+
+class MatrixAddition1Tester(dut: MatrixAddition1)
+    extends PeekPokeTester(dut) {
+  for (i <- 0 until 3 * 2) {
+    poke(dut.io.a(i), i)
+    poke(dut.io.b(i), i)
+  }
+  step(1)
+  for (i <- 0 until 3 * 2) {
+    print(dut.io.out(i))
+    print(' ')
+    expect(dut.io.out(i), i * 2)
+  }
+}
+
+object MatrixAddition1Tester extends App {
+  chisel3.iotesters.Driver(() => new MatrixAddition1(3, 2)) { dut =>
+    new MatrixAddition1Tester(dut)
+  }
+}
