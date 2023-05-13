466470121f61749fbf5e34c6bd0bcbf360f3e7b8
txz <920504276@qq.com>
2023-05-13 15:41:20 +0800

    null

--- src/main/scala/madd/FIFOIO.scala
+++ src/main/scala/madd/FIFOIO.scala
@@ -6,16 +6,16 @@ import chisel3.util._
 class FIFOIO(val pcWidth: UInt, val addressWidth: UInt) extends Bundle {
     // 输入端口
     val enIn = Input(Bool())
-    val pcIn = Input(UInt(pcWidth.W))
+    val pcIn = Input(UInt(pcWidth))
 	val enWrite = Input(Bool())
-	val addressIn = Input(UInt(addressWidth.W))
-    val strideIn = Output(SInt(addressWidth.W+1))
+	val addressIn = Input(UInt(addressWidth))
+    val strideIn = Output(SInt(addressWidth+1.U))
 	
     // 输出端口
-    val addressOut = Output(UInt(addressWidth.W))
-    val strideOut = Output(SInt(addressWidth.W+1))
+    val addressOut = Output(UInt(addressWidth))
+    val strideOut = Output(SInt(addressWidth+1.U))
 	val find = Output(Bool())
-	
+
   override def cloneType =
     new FIFOIO(pcWidth, addressWidth).asInstanceOf[this.type]
 }
--- src/main/scala/madd/prefetchIO.scala
+++ src/main/scala/madd/prefetchIO.scala
@@ -5,10 +5,10 @@ import chisel3.util._
 
 class PrefetchIO(val pcWidth: UInt,val addressWidth: UInt) extends Bundle {
     // 输入端口
-    val pc = Input(UInt(pcWidth.W))
-    val address = Input(UInt(addressWidth.W))
+    val pc = Input(UInt(pcWidth))
+    val address = Input(UInt(addressWidth))
     // 输出端口
-    val prefetch_address = Output(UInt(addressWidth.W))
+    val prefetch_address = Output(UInt(addressWidth))
     val prefetch_valid = Output(Bool())
 
   override def cloneType =
