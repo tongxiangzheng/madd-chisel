181c8eac19a66b8f08fb4ec4b7fa8b2f988d4fc4
txz <920504276@qq.com>
2023-05-13 16:41:44 +0800

    null

--- src/main/scala/madd/FIFO.scala
+++ src/main/scala/madd/FIFO.scala
@@ -4,12 +4,6 @@ import chisel3._
 import chisel3.util._
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
-class ItemData(val pcWidth: Int,val addressWidth: Int) extends Bundle {
-    val pc = UInt(pcWidth.W)
-    val address = UInt(addressWidth.W)
-	val stride = SInt(addressWidth.W+1) //SInt 记得大小加一
-}
-
 class FIFO(val size: Int,val pcWidth: Int,val addressWidth: Int) extends Module {
   val io = IO(new FIFOIO(pcWidth,addressWidth))
   /*val queueWire = Wire(Vec(size,new ItemData(pcWidth,addressWidth)))
