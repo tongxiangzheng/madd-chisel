d700591debcee9b8d0d63f99200db778503abd47
txz <920504276@qq.com>
2023-05-21 20:56:07 +0800

    null

--- src/main/scala/madd/prefetchTester.scala
+++ src/main/scala/madd/prefetchTester.scala
@@ -10,7 +10,7 @@ class PrefetchTester(dut: Prefetch)
   val numAccesses = 16
   val testPointNum = 3
   var trace = new Array[Array[(Int, Int)]](3)
-  val tracename = Array("顺序", "完全随机", "概率顺序")
+  val tracename = Array("顺序", "完全随机", "概率顺序","pc值随机")
   for (i <- 0 until testPointNum) {
     //(pc,address)
     trace(i) = new Array[(Int, Int)](numAccesses)
@@ -34,6 +34,7 @@ class PrefetchTester(dut: Prefetch)
       scala.Predef.printf(s"[Tester] pc: ${trace(i)(j)._1} address: ${trace(i)(j)._2} valid: ${peek(dut.io.prefetch_valid)} prefetch_address: ${peek(dut.io.prefetch_address)} \n");
       
       poke(dut.io.pc, 0)
+      step(1)
     }
     
     scala.Predef.printf(s"[Tester] testName: ${tracename(i)}\n");
