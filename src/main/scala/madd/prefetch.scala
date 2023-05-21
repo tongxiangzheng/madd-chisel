496298a9f936575add9ebf64b8f8ce939ad1ebe2
txz <920504276@qq.com>
2023-05-21 16:32:05 +0800

    null

--- src/main/scala/madd/prefetch.scala
+++ src/main/scala/madd/prefetch.scala
@@ -7,8 +7,10 @@ import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 class ItemData(val pcWidth: Int,val addressWidth: Int) extends Bundle {
   val pc = UInt(pcWidth.W)
   val address = UInt(addressWidth.W)
-	val stride = SInt(addressWidth.W+1) //SInt 记得大小加一
   val timestamp = UInt(32.W)
+	val stride = UInt(addressWidth.W)
+  val haveStride = Bool()
+  val reliability = UInt(32.W)
 }
 
 class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
@@ -18,32 +20,44 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
   val lastPC = RegInit(0.U(pcWidth.W))
   val prefetch_valid = RegInit(false.B)
   val prefetch_address = RegInit(0.U(addressWidth.W))
+  //val stride = RegInit(0.U(addressWidth.W))
+  //val reliability = RegInit(0.U(32.W))
+  
   val queueWire = Wire(Vec(size,new ItemData(pcWidth,addressWidth)))
   for (i <- 0 until size) {
 	  queueWire(i).pc:=0.U(pcWidth.W)
 	  queueWire(i).address:=0.U(addressWidth.W)
 	  queueWire(i).stride:=0.S(addressWidth.W+1)
     queueWire(i).timestamp:=0.U(32.W)
+    queueWire(i).haveStride:=false
   }
   val queueReg = RegInit(queueWire)
+
+
+//start
 
   var enable = io.pc =/= lastPC
   enable = Mux(io.pc===0.U,false.B,enable)
   lastPC:=io.pc
   when(enable){
     var p=fifoFind(io.pc)
-    when(p===size.U){
-     prefetch_valid:=false.B
-     prefetch_address:=DontCare
+    var found = (p=/=size.U)
+    prefetch_valid:=found
+    var stride=0.U,reliability=0.U
+    when(found){
+      var newStride=io.address-queueReg(p).address
+
+      reliability=calcReliability(queueReg(p).stride,queueReg(p).reliability,newStride)
+      val replace=(reliability===0)
+      stride=Mux(replace,newStride,queueReg(p).stride)
+      prefetch_address:=io.address+stride
     }.otherwise{
-      prefetch_valid:=true.B
-      prefetch_address:=queueReg(p).address
+      prefetch_address:=0.U
     }
-   
     /*chisel3.printf(
       p"main: p: ${p} pc: ${io.pc} prefetch_valid: ${io.prefetch_valid} prefetch_address: ${io.prefetch_address}\n"
     )*/
-    fifoWrite(io.pc,io.address,10.S,enable)
+    fifoWrite(io.pc,io.address,stride,reliability,found)
   }
   io.prefetch_valid:=prefetch_valid
   io.prefetch_address:=prefetch_address
@@ -62,9 +76,9 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
     }
     p
   }
-  def fifoWrite(pc:UInt,address:UInt,stride:SInt,enable:Bool):Unit = {
+  def fifoWrite(pc:UInt,address:UInt,stride:UInt,reliability:UInt,haveStride:Bool):Unit = {
     var p=0.U
-    var found=Mux(enable,false.B,true.B)
+    var found=false.B
     //是否有该项
     for(i <- 0 until size){
       val check=(queueReg(i).pc===pc)
@@ -100,8 +114,17 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
     queueReg(p).pc:=pc
     queueReg(p).address:=address
     queueReg(p).stride:=stride
+    queueReg(p).haveStride:=haveStride
+    queueReg(p).reliability:=reliability
     dfn:=dfn+1.U
     queueReg(p).timestamp:=dfn
+  }
+  def calcReliability(stride:UInt,reliability:UInt,newStride:UInt):UInt={
+    //返回可信度，为0表示替换为newStride
+    val same=(stride===newstride)
+    var ans=reliability+1.U
+    var solve=same;
+    ans=Mux(solve,ans,reliability>>1)
   }
 }
 
