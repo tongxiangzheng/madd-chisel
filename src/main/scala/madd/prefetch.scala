6cf692aba652bab0b89ba59f1893ee99a0d4d299
txz <920504276@qq.com>
2023-05-21 14:59:57 +0800

    null

--- src/main/scala/madd/prefetch.scala
+++ src/main/scala/madd/prefetch.scala
@@ -14,7 +14,7 @@ class ItemData(val pcWidth: Int,val addressWidth: Int) extends Bundle {
 class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
   val io = IO(new PrefetchIO(pcWidth,addressWidth))
   val size = 8
-  val dfn = RegInit().U(32.W))
+  val dfn = RegInit(0.U(32.W))
   val queueWire = Wire(Vec(size,new ItemData(pcWidth,addressWidth)))
   for (i <- 0 until size) {
 	  queueWire(i).pc:=0.U(pcWidth.W)
