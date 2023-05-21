e18265e7787d1bf9037aa99fbeaf5845101b32d0
txz <920504276@qq.com>
2023-05-21 22:54:07 +0800

    null

--- src/main/scala/madd/prefetch.scala
+++ src/main/scala/madd/prefetch.scala
@@ -134,7 +134,7 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
   val prereliability = RegInit(0.U(32.W))
   val replace = RegInit(false.B)
 
-  chisel3.printf(p"reliability: ${reliability} stride: ${stride} prereliability: ${prereliability}\n");
+  chisel3.printf(p"replace: ${replace} reliability: ${reliability} stride: ${stride} prereliability: ${prereliability}\n");
   when(enable){
     var p=fifoFind(io.pc)
     var found = (p=/=size.U)
