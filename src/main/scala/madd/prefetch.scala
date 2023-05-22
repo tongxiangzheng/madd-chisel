aabc225a045c123e62dc81ed13f15f4aba81b73f
txz <920504276@qq.com>
2023-05-22 19:29:03 +0800

    null

--- src/main/scala/madd/prefetch.scala
+++ src/main/scala/madd/prefetch.scala
@@ -136,7 +136,7 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
   //val reliability = RegInit(0.U(32.W))
   //val prereliability = RegInit(0.U(32.W))
   //val replace = RegInit(false.B)
-  //chisel3.printf(p"0 pc: ${queueReg(0).pc} address: ${queueReg(0).address} stride: ${queueReg(0).stride} reliability: ${queueReg(0).reliability} \n");
+  chisel3.printf(p"0 pc: ${queueReg(0).pc} address: ${queueReg(0).address} stride: ${queueReg(0).stride} reliability: ${queueReg(0).reliability} \n");
   //chisel3.printf(p"io.enable: ${io.enable} unblock: ${unblock} \n");
   
   //chisel3.printf(p"enable: ${enable} replace: ${replace} reliability: ${reliability} stride: ${stride} newStride: ${newStride} prereliability: ${prereliability}\n");
