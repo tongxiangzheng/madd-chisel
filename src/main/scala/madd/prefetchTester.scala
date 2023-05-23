7192f004c49abba6300226d611ef8ef981ee4df8
txz <920504276@qq.com>
2023-05-23 22:40:41 +0800

    null

--- src/main/scala/madd/prefetchTester.scala
+++ src/main/scala/madd/prefetchTester.scala
@@ -96,7 +96,7 @@ class PrefetchTester(dut: Prefetch)
 }
 
 object PrefetchTester extends App {
-  println((new ChiselStage).emitVerilog(new Prefetch(32, 32)))
+  scala.Predef.println((new ChiselStage).emitVerilog(new Prefetch(32, 32)))
   /*chisel3.iotesters.Driver(() => new Prefetch(32, 32)) { dut =>
     new PrefetchTester(dut)
   }*/
