cc9590e3707499e7de03d30974f1a45f277a02bc
txz <920504276@qq.com>
2023-05-23 22:09:12 +0800

    null

--- src/main/scala/madd/prefetchVerilog.scala
+++ src/main/scala/madd/prefetchVerilog.scala
@@ -1,10 +1,11 @@
 import chisel3.stage.ChiselGeneratorAnnotation
 import circt.stage.{ChiselStage, FirtoolOption}
 
-/*(new ChiselStage).execute(
+/*
+)*/
+object PrefetchVerilog extends App {
+  (new ChiselStage).execute(
   Array("--target", "systemverilog"),
   Seq(ChiselGeneratorAnnotation(() => new Prefetch(32, 32)),
     FirtoolOption("--disable-all-randomization"))
-)*/
-val verilogString = chisel3.getVerilogString(new Prefetch(32, 32))
-println(verilogString)
+}
