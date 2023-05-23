98826b8521f7022791df4b64a2f334ca444c06aa
txz <920504276@qq.com>
2023-05-23 22:09:56 +0800

    null

--- src/main/scala/madd/prefetchVerilog.scala
+++ src/main/scala/madd/prefetchVerilog.scala
@@ -2,10 +2,10 @@ import chisel3.stage.ChiselGeneratorAnnotation
 import circt.stage.{ChiselStage, FirtoolOption}
 
 /*
-)*/
+*/
 object PrefetchVerilog extends App {
   (new ChiselStage).execute(
   Array("--target", "systemverilog"),
   Seq(ChiselGeneratorAnnotation(() => new Prefetch(32, 32)),
-    FirtoolOption("--disable-all-randomization"))
+    FirtoolOption("--disable-all-randomization")))
 }
