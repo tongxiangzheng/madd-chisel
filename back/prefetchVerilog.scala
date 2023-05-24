27cbe081369458f0d5024b42e0a63c8ddb4bd309
txz <920504276@qq.com>
2023-05-24 18:50:38 +0800

    null

---
+++ back/prefetchVerilog.scala
@@ -0,0 +1,15 @@
+package madd
+
+import chisel3._
+import chisel3.util._
+
+import chisel3.stage.ChiselGeneratorAnnotation
+import chisel3.stage.ChiselStage
+
+/*
+*/
+
+object PrefetchVerilog extends App {
+  
+  scala.Predef.printf((new chisel3.stage.ChiselStage).emitVerilog(new Prefetch(32, 32)))
+}
