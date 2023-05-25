702c5e3ec890b78922f9dc7e0ea13adcd41d382a
txz <920504276@qq.com>
2023-05-25 18:37:05 +0800

    null

--- src/main/scala/madd/prefetch.scala
+++ src/main/scala/madd/prefetch.scala
@@ -192,7 +192,7 @@ class Prefetch(val pcWidth: Int,val addressWidth: Int) extends Module {
 }
 object Prefetch extends App {
   (new ChiselStage).execute(
-    Array("-X", "verilog", "-td", "source/"),
+    Array("-X", "verilog", "-td", "source/","-disable-all-randomization", "-strip-debug-info"),
     Seq(
       ChiselGeneratorAnnotation(() => new Cache1())
     )
